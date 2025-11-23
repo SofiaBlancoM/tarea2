7a32a6abacc2ba72f73b3108ca6ef82cc6383529
Sofia Blanco <4870998@gmail.com>
2025-11-23 18:42:22 +0100

    feat: flow loop on the principal menu implementation

--- src/main/java/es/cifpcarlos3/Main.java
+++ src/main/java/es/cifpcarlos3/Main.java
@@ -2,6 +2,8 @@ package es.cifpcarlos3;
 
 import es.cifpcarlos3.model.Module;
 import es.cifpcarlos3.ui.MenuManager;
+import es.cifpcarlos3.ui.MenuOption;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
@@ -23,8 +25,35 @@ public class Main {
 
         System.out.println("Bienvenido/a a la segunda tarea de Acceso a Datos");
 
-        MenuManager.getSelectedOption();
+        MenuOption selectedOption = MenuManager.getSelectedOption();
 
+        while (selectedOption != MenuOption.EXIT) {
+
+            switch (selectedOption) {
+                case GET_MODULES:
+                    System.out.println("Listando módulos...");
+                    break;
+                case ADD_PROFESSOR:
+                    System.out.println("Introduce el DNI");
+                    System.out.println("Introduce el nombre");
+                    System.out.println("Introduce los apellidos");
+                    System.out.println("Introduce el número de teléfono");
+                    break;
+                case DELETE_PROFESSOR:
+                    System.out.println("Introduce el DNI del profesor");
+                    break;
+                case GET_ALL_PROFESSORS:
+                    System.out.println("Listando profesores");
+                    break;
+
+            }
+
+            selectedOption = MenuManager.getSelectedOption();
+
+        }
+
+        System.out.println("¡Gracias por utilizar nuestra aplicación, esperamos volver a verte pronto!");
+        System.out.println("Cerrando...");
 
     }
 
